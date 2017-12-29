2c1b5d08b98c739bb059950afc2b0eca1ae40382
tinko's pi <dingguo.tu@gmail.com>
2017-12-29 12:33:42 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,59 @@
+# ddns.py
+
+```
+@author tinko
+@version 0.0.1
+```
+
+ddns.py 是基于 [DNSPod](http://www.dnspod.cn/docs/records.html#dns) 服务的动态 DNS 脚本，用于检测 IP 变化并更新至 DNSPod，支持多域名解析。支持 Linux 设备，包括树莓派（[Raspberry Pi](https://www.raspberrypi.org/)）。
+
+# Prerequisites
+
+1. python
+
+# Installation
+
+安装 [git](https://git-scm.com/) 客户端，通过本命令获取 ddns.py
+
+<pre>
+git clone https://github.com/migege/dnspod.git dnspod
+</pre>
+
+然后到 ddns 目录下新建 ```conf.json``` 文件，根据您的 DNSPod 设置，填入以下内容：
+
+<pre>
+{
+    "id": &lt;your_api_token_id&gt,
+    "token": &lt;your_api_token&gt,
+    "domain": &lt;your_domain&gt,
+    "sub_domains": [
+	{
+	    "name": &lt;your_first_sub_domain_name&gt;,
+	    "domain_id": &lt;your_domain_id&gt;,
+	    "record_id": &lt;your_record_id&gt;
+	},
+	{
+	    "name": &lt;your_second_sub_domain_name&gt;,
+	    "domain_id": &lt;your_domain_id&gt;,
+	    "record_id": &lt;your_record_id&gt;
+	}
+    ]
+}
+</pre>
+
+获取你的domain_id
+<pre>
+curl -X POST https://dnsapi.cn/Domain.List -d 'login_token=LOGIN_TOKEN&format=json'
+</pre>
+
+获取你的record_id
+<pre>
+curl -X POST https://dnsapi.cn/Record.List -d 'login_token=LOGIN_TOKEN&format=json&domain_id=YOUR_DOMAIN_ID'
+</pre>
+
+最后设置 crontab 定时任务
+每隔30分钟运行一次，以便更新DNS记录：
+<pre>
+sudo crontab -e
+/30 * * * * python /home/pi/ddns/ddns.py
+</pre>
---
+++ ddns.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python
+
+# -*- coding:utf-8 -*-
+
+import socket 
+import json
+import os
+
+ID = None
+Token = None
+Domain = None
+SubDomains = None
+
+params = dict(
+    login_token=None,
+    format="json",
+    domain_id=None,
+    record_id=None,
+    sub_domain=None,
+    record_line="默认",
+    value=None
+)
+
+def ddns(ip):
+    import httplib 
+    import urllib
+    
+    params['login_token'] = ("%s,%s" % (ID, Token))
+    params['value'] = ip
+    
+    headers = {"Content-type": "application/x-www-form-urlencoded", "Accept": "text/json"}
+    conn = httplib.HTTPSConnection("dnsapi.cn")
+
+    for item in SubDomains:
+        params['domain_id'] = item['domain_id']
+        params['record_id'] = item['record_id']
+        params['sub_domain'] = item['name']
+        
+        conn.request("POST", "/Record.Ddns", urllib.urlencode(params), headers)
+        response = conn.getresponse()
+        # logger().info(response.status, response.reason)
+        data = json.loads(response.read())
+        if int(data['status']['code']) == 1:
+            current_ip = ip
+            print ("DDns Success for subdomain [%s], IP change to %s" % (item['name'], ip))
+            continue
+        else:
+            print ("DDns Error for subdomain [%s]: %s" % (item['name'], data['status']['message']))
+            continue
+    conn.close()
+    
+    
+
+def logger():
+    import logging
+    import datetime
+    LOG_FORMAT = "[%(asctime)s]\t[%(levelname)s]\t[%(message)s]"
+    LOG_FILE = os.path.join(os.path.dirname(os.path.realpath(__file__)), ("%s%s%s.log" % (i.day, i.month, i.year) ))
+    logging.basicConfig(format=LOG_FORMAT, level=logging.DEBUG, filename=LOG_FILE)
+    return logging.getLogger(__name__)
+
+    
+def getip():
+    sock = socket.create_connection(('ns1.dnspod.net', 6666), 20)
+    ip = sock.recv(16)
+    sock.close()
+    return ip
+
+
+def getHostIP(domain):
+    ip = socket.gethostbyname_ex(domain)
+    return ip[2][0]
+
+    
+if __name__ == '__main__':
+    conf = json.load(open(os.path.join(os.path.dirname(os.path.realpath(__file__)), "conf.json"), "r"))
+
+    ID = conf['id']
+    Token = conf['token']
+    Domain = conf['domain']
+    SubDomains = conf['sub_domains']
+    
+    try:
+        ip = getip()
+        hostip = getHostIP(Domain)
+        print ip, hostip
+        if hostip != ip:
+            ddns(ip)
+    except Exception as e:
+        print e
+        pass
