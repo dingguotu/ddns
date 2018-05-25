230327a4b2185f3fbc302531ed968345e602f702
Dingguo.Tu <Dingguo.Tu@starlight.com>
2018-05-25 16:35:43 +0800

    add multi-domain support

--- ddns.py
+++ ddns.py
@@ -1,66 +1,27 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
-
 import socket 
 import json
 import os
 
 global LocalIP
 global HostIP
+global Login_Token
+Domains = None
 
-ID = None
-Token = None
-Domain = None
-SubDomains = None
+def ddns(domain):
+    import dnspod
 
-params = dict(
-    login_token=None,
-    format="json",
-    domain_id=None,
-    record_id=None,
-    sub_domain=None,
-    record_line="默认",
-    value=None
-)
-
-def ddns():
-    import httplib 
-    import urllib
-
-    print ("LocalIP is: %s, HostIP is: %s" % (LocalIP, HostIP))
+    domain_id = dnspod.get_domain_id(Login_Token, domain['name'])
+    if domain_id == 0:
+        domain_id = dnspod.create_domain(Login_Token, domain['name'])
     
-    params['login_token'] = ("%s,%s" % (ID, Token))
-    params['value'] = LocalIP
-    
-    headers = {"Content-type": "application/x-www-form-urlencoded", "Accept": "text/json"}
-    conn = httplib.HTTPSConnection("dnsapi.cn")
-
-    for item in SubDomains:
-        params['domain_id'] = item['domain_id']
-        params['record_id'] = item['record_id']
-        params['sub_domain'] = item['name']
-        
-        conn.request("POST", "/Record.Ddns", urllib.urlencode(params), headers)
-        response = conn.getresponse()
-        # logger().info(response.status, response.reason)
-        data = json.loads(response.read())
-        if int(data['status']['code']) == 1:
-            print ("DDns Success for subdomain [%s], IP change to %s" % (item['name'], LocalIP))
-            continue
+    for sub_domain in domain['sub_domains']:
+        record_id = dnspod.get_record_id(Login_Token, domain_id, sub_domain)
+        if record_id == 0:
+            dnspod.create_record_id(Login_Token, domain_id, sub_domain, LocalIP)
         else:
-            print ("DDns Error for subdomain [%s]: %s" % (item['name'], data['status']['message']))
-            continue
-    conn.close()
-    
-    
-
-def logger():
-    import logging
-    import datetime
-    LOG_FORMAT = "[%(asctime)s]\t[%(levelname)s]\t[%(message)s]"
-    LOG_FILE = os.path.join(os.path.dirname(os.path.realpath(__file__)), ("%s%s%s.log" % (i.day, i.month, i.year) ))
-    logging.basicConfig(format=LOG_FORMAT, level=logging.DEBUG, filename=LOG_FILE)
-    return logging.getLogger(__name__)
+            dnspod.record_ddns(Login_Token, domain_id, record_id, sub_domain, LocalIP)
 
     
 def getip():
@@ -70,25 +31,26 @@ def getip():
     sock.close()
 
 
-def getHostIP():
+def getHostIP(domain):
     global HostIP
-    ip = socket.gethostbyname_ex(Domain)
+    ip = socket.gethostbyname_ex(domain['name'])
     HostIP = ip[2][0]
 
     
 if __name__ == '__main__':
+    global Login_Token
     conf = json.load(open(os.path.join(os.path.dirname(os.path.realpath(__file__)), "conf.json"), "r"))
 
-    ID = conf['id']
-    Token = conf['token']
-    Domain = conf['domain']
-    SubDomains = conf['sub_domains']
+    Login_Token = "%s,%s" % (conf['id'], conf['token'])
+    Domains = conf['domains']
     
     try:
         getip()
-        getHostIP()
-        if HostIP != LocalIP:
-            ddns()
+        for domain in Domains:
+            getHostIP(domain)
+            if HostIP != LocalIP:
+                print "Begin update [%s]." % domain['name']
+                ddns(domain)
     except Exception as e:
         print e
         pass
---
+++ dnspod.py
@@ -0,0 +1,113 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+import httplib 
+import urllib
+import json
+import os
+
+Headers = {"Content-type": "application/x-www-form-urlencoded", "Accept": "text/json"}
+
+def get_domain_id(login_token, domain_name):
+    conn = httplib.HTTPSConnection("dnsapi.cn")
+    params = dict(
+        login_token=login_token,
+        format="json"
+    )
+    conn.request("POST", "/Domain.List", urllib.urlencode(params), Headers)
+    response = conn.getresponse()
+    data = json.loads(response.read())
+
+    if int(data['status']['code']) == 1:
+        domails = data['domains']
+        for domain in domails:
+            if domain['name'] == domain_name:
+                return domain['id']
+    else:
+        return 0
+
+    conn.close()
+
+
+def create_domain(login_token, domain_name):
+    conn = httplib.HTTPSConnection("dnsapi.cn")
+    params = dict(
+        login_token=login_token,
+        format="json",
+        domain = domain_name
+    )
+    conn.request("POST", "/Domain.Create", urllib.urlencode(params), Headers)
+    response = conn.getresponse()
+    data = json.loads(response.read())
+
+    if int(data['status']['code']) == 1:
+        return data['domain']['id']
+
+    conn.close()
+
+
+def get_record_id(login_token, domain_id, sub_domain):
+    conn = httplib.HTTPSConnection("dnsapi.cn")
+    params = dict(
+        login_token=login_token,
+        format="json",
+        domain_id = domain_id,
+        sub_domain = sub_domain
+    )
+    conn.request("POST", "/Record.List", urllib.urlencode(params), Headers)
+    response = conn.getresponse()
+    data = json.loads(response.read())
+
+    if int(data['status']['code']) == 1:
+        records = data['records']
+        for record in records:
+            if record['type'] == 'A' and record['name'] == sub_domain:
+                return record['id']
+    else:
+        return 0
+
+    conn.close()
+
+
+def create_record_id(login_token, domain_id, sub_domain, localIP):
+    conn = httplib.HTTPSConnection("dnsapi.cn")
+    params = dict(
+        login_token=login_token,
+        format="json",
+        domain_id = domain_id,
+        sub_domain = sub_domain,
+        record_type = 'A',
+        record_line_id = "0",
+        value = localIP
+    )
+    conn.request("POST", "/Record.Create", urllib.urlencode(params), Headers)
+    response = conn.getresponse()
+    data = json.loads(response.read())
+
+    if int(data['status']['code']) == 1:
+        print ("Sub_domain [%s] create success" % sub_domain)
+
+    conn.close()
+
+
+def record_ddns(login_token, domain_id, record_id, sub_domain, localIP):
+    conn = httplib.HTTPSConnection("dnsapi.cn")
+    params = dict(
+        login_token=login_token,
+        format="json",
+        domain_id = domain_id,
+        record_id = record_id,
+        sub_domain = sub_domain,
+        record_line_id = "0",
+        value = localIP
+    )
+
+    conn.request("POST", "/Record.Ddns", urllib.urlencode(params), Headers)
+    response = conn.getresponse()
+    data = json.loads(response.read())
+
+    if int(data['status']['code']) == 1:
+        print ("DDns Success for subdomain [%s], IP change to %s" % (sub_domain, localIP))
+    else:
+        print ("DDns Error for subdomain [%s]: %s" % (sub_domain, data['status']['message']))
+
+    conn.close()
